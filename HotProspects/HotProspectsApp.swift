47991d6fe5cdc99d5024a0f096b87d6204fbb999
LuiArm <Luisarmendariz24@icloud.com>
2023-08-21 16:15:56 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by luis armendariz on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
