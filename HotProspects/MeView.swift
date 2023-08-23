190aea3e3692d9885f74fa63f0792e7e4fcd4824
LuiArm <Luisarmendariz24@icloud.com>
2023-08-23 13:56:00 -0600

    add push notifications and finish app

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
 
     
     let context = CIContext()
@@ -27,14 +28,29 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu{
+                        Button{
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        }label: {
+                            Label("Save Image", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
+    }
+    
+    func updateCode(){
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage{
@@ -43,6 +59,7 @@ struct MeView: View {
         if let outputImage = filter.outputImage{
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
                 return UIImage(cgImage: cgimg)
+                
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
