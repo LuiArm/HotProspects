70b236d4e3088ad8801ec414a97496f7314911a2
LuiArm <Luisarmendariz24@icloud.com>
2023-08-22 22:56:44 -0600

    Add prospect views and generate qr code. add tab view and tab items

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,29 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by luis armendariz on 8/22/23.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+    }
+}
